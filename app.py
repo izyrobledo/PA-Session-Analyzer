ca27e9cb1218f6dd373bb46b6c47d083c22e9143
Kevin Zarzana <kevinzarzana@Kevins-MacBook-Pro-2.local>
2017-08-07 15:38:20 -0500

    Sending Things

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 app = Flask(__name__)
- 
+
 @app.route("/")
 def index():
     return "Flask App!"
- 
+
 # @app.route("/hello")
 # def hello():
 #     return "Hello World!"
- 
+
 @app.route("/members")
 def members():
     return "Members"
- 
+
 
 @app.route("/hello/Isabella/", methods = ["GET", "POST"])
 def hello():
     return render_template(
         'form.html')
 
- 
+@app.route("/hello/Isabella/response/", methods = ["GET", "POST"])
+def response():
+    choice=request.form['userChoice']
+    environment=request.form['env']
+    ssid=request.form['SSID']
+    email=request.form['youremail']
+    return render_template('response.html', choice=choice, environment=environment, ssid=ssid, email=email)
+
+
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=80)
     app.run()
- 
+
 
 
 
