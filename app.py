407d98d955587db172eaea8a0a77cc40b8fb79b5
Kevin Zarzana <kevinzarzana@Kevins-MacBook-Pro-2.local>
2017-08-08 09:34:52 -0500

    I did.

--- app.py
+++ app.py
@@ -18,24 +18,17 @@ def members():
 def hello():
     return render_template(
         'form.html')
-    
+
 
 @app.route("/hello/Isabella/response/", methods = ["GET", "POST"])
 def response():
-<<<<<<< HEAD
-	userChoice = request.form
-	return render_template(
-		'response.html')
- 
-=======
     choice=request.form['userChoice']
     environment=request.form['env']
     ssid=request.form['SSID']
     email=request.form['youremail']
+    import get_AWSInfo.py
     return render_template('response.html', choice=choice, environment=environment, ssid=ssid, email=email)
 
-
->>>>>>> ca27e9cb1218f6dd373bb46b6c47d083c22e9143
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=80)
     app.run()
