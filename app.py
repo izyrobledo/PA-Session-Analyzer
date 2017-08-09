66f195fd18c7f2dbb8a80f7a419267bdaf6b74ca
Kevin Zarzana <kevinzarzana@Kevins-MacBook-Pro-2.local>
2017-08-09 09:19:06 -0500

    Kev

--- app.py
+++ app.py
@@ -14,20 +14,19 @@ def members():
     return "Members"
 
 
-@app.route("/hello/Isabella/", methods = ["GET", "POST"])
+@app.route("/form/fillOut/", methods = ["GET", "POST"])
 def hello():
-    return render_template(
-        'form.html')
+    return render_template('Formv2.html')
 
 
-@app.route("/hello/Isabella/response/", methods = ["GET", "POST"])
+@app.route("/form/response/", methods = ["GET", "POST"])
 def response():
     choice=request.form['userChoice']
     environment=request.form['env']
     ssid=request.form['SSID']
-    email=request.form['youremail']
-    import get_AWSInfo.py
-    return render_template('response.html', choice=choice, environment=environment, ssid=ssid, email=email)
+    return render_template('Responsev2.html', choice=choice, environment=environment, ssid=ssid)
+
+
 
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=80)
